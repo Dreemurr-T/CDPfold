d64b2f28f03687f1cae6bd90edc0686fe01369b4
Dreemurr-T <1305543200@qq.com>
2021-12-04 10:46:19 +0800

    fix preprocessing bugs

--- data_pre_treatment.py
+++ data_pre_treatment.py
@@ -9,22 +9,28 @@ import os
 import pandas as pd
 import csv
 
- # 遍历指定目录，显示目录下的所有文件名
+# 遍历指定目录，显示目录下的所有文件名
 
-def readfile(fileDir,filename):
-    fopen = open(fileDir+filename,'r')
+list_Dir = 'C:/Users/thy01/Desktop/AI_group/archiveII/' # 修改为本地文件路径
+csv_Dir = 'C:/Users/thy01/Desktop/AI_group/data_csv/'
+seq_Dir = 'C:/Users/thy01/Desktop/AI_group/data_seq/'
+
+
+def readfile(fileDir, filename):
+    fopen = open(fileDir+filename, 'r')
     rnafile = fopen.readlines()
     del rnafile[0]
-    with open("test.txt",'w') as f:
+    with open("test.txt", 'w') as f:
         for i in rnafile:
             f.write(i)
     data = pd.read_csv("test.txt", sep='\t', header=None)
-    return data,filename
+    return data, filename
 
-def transform(data,filename):
-    rnaseq = data.loc[:,1]
-    rnadata1 = data.loc[:,0]
-    rnadata2 = data.loc[:,4]
+
+def transform(data):
+    rnaseq = data.loc[:, 1]
+    rnadata1 = data.loc[:, 0]
+    rnadata2 = data.loc[:, 4]
     rnastructure = []
     for i in range(len(rnadata2)):
         if rnadata2[i] == 0:
@@ -34,35 +40,36 @@ def transform(data,filename):
                 rnastructure.append(")")
             else:
                 rnastructure.append("(")
-    return rnaseq,rnastructure,filename
+    return rnaseq, rnastructure
 
-def savefile(rnaseq,rnastructure,filename):
+
+def savefile(rnaseq, rnastructure, filename):
+    if not os.path.exists(csv_Dir):
+        os.mkdir(csv_Dir)
     rnafile = filename[:-3]
     rnafile = rnafile+".csv"
     rnafile = csv_Dir + rnafile
-    rnacsv = open(rnafile,'wb')
+    rnacsv = open(rnafile, 'w', newline="")
     writer = csv.writer(rnacsv)
     m = len(rnastructure)
     for i in range(m):
+        # print(rnastructure)
         writer.writerow(rnastructure[i])
-    os.remove("test.txt")
+    if not os.path.exists(seq_Dir):
+        os.mkdir(seq_Dir)
     rnaseqfile = seq_Dir + filename[:-3] + "_seq.csv"
-    rnaseqcsv = open(rnaseqfile,'wb')
+    rnaseqcsv = open(rnaseqfile, 'w', newline="")
     seqwriter = csv.writer(rnaseqcsv)
     m = len(rnastructure)
     for i in range(m):
         seqwriter.writerow(rnaseq[i])
-        
-list_Dir = '/home/zhangch/RSS/source_data/' 
-csv_Dir = '/home/zhangch/RSS/stru_data/' 
-seq_Dir = '/home/zhangch/RSS/seq_data/' 
-   
-if __name__ == '__main__':
-    pathDir =  os.listdir(list_Dir)
-    for i in pathDir:
-        data,filename=readfile(list_Dir , i)
-        rnaseq,rnastructure,filename=transform(data,filename)
-        savefile(rnaseq,rnastructure,filename)
-    
 
-    
+
+if __name__ == '__main__':
+    pathDir = os.listdir(list_Dir)
+    for i in pathDir:
+        if i.endswith(".ct") and i.split('_')[0] != 'telomerase':
+            # print(i)
+            data, filename = readfile(list_Dir, i)
+            rnaseq, rnastructure = transform(data)
+            savefile(rnaseq, rnastructure, filename)
